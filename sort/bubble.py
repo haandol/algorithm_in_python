512591ac43474995371b8c2778b14128c7717f77
haandol <ldg55d@gmail.com>
2017-02-14 15:44:59 +0900

    add selection sort

--- sort/bubble.py
+++ sort/bubble.py
@@ -2,12 +2,13 @@
 
 def sort(L):
     n = len(L)
-    for i in range(n-1, -1, -1):
-        for j in range(i):
+    for i in xrange(n-1, -1, -1):
+        for j in xrange(i):
             if L[j] > L[j+1]:
                 L[j], L[j+1] = L[j+1], L[j]
 
 
-L = [20, 30, 40, 90, 50, 60, 70, 80, 100, 110]
-sort(L)
-assert L == sorted(L)
+if '__main__' == __name__:
+    L = [20, 30, 40, 90, 50, 60, 70, 80, 100, 110]
+    sort(L)
+    assert L == sorted(L)
---
+++ sort/selection.py
@@ -0,0 +1,18 @@
+# https://interactivepython.org/runestone/static/pythonds/SortSearch/TheSelectionSort.html
+
+def sort(L):
+    n = len(L)
+    for slot_pos in xrange(n-1, -1, -1):
+        max_pos = slot_pos
+        for j in xrange(slot_pos):
+            if L[max_pos] < L[j]:
+                max_pos = j
+
+        L[max_pos], L[slot_pos] = L[slot_pos], L[max_pos]
+
+
+if '__main__' == __name__:
+    L = [20, 30, 40, 90, 50, 60, 70, 80, 100, 110]
+    sort(L)
+    print L
+    assert L == sorted(L)
